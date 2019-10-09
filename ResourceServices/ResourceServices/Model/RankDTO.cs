a22cea837e190fb0747e1db59a9132b7928a4360
HUY-NGUYEN\huy <huy.nguyen2@wisky.vn>
2019-10-09 21:58:59 +0700

    configuration Test

---
+++ ResourceServices/ResourceServices/Model/RankDTO.cs
@@ -0,0 +1,34 @@
+﻿using AuthenServices.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResourceServices.Model
+{
+    [JsonObject("RankDTO", ItemNullValueHandling = NullValueHandling.Ignore)]
+    public class RankDTO
+    {
+        [JsonProperty("RankId")]
+        public int? RankId { get; set; }
+        public string name { get; set; }
+        public DateTime? creatAt { get; set; }
+        public DateTime? updateAt { get; set; }
+        public bool? isActive { get; set; }
+
+        public RankDTO()
+        {
+
+        }
+
+        public RankDTO(Rank rank)
+        {
+            this.RankId = rank.RankId;
+            this.name = rank.Name;
+            this.creatAt = rank.CreateAt;
+            this.updateAt = rank.UpdateAt;
+            this.isActive = rank.IsActive;
+        }
+    }
+}
--- ResourceServices/ResourceServices/Service/CompanyDAO.cs
+++ ResourceServices/ResourceServices/Service/CompanyDAO.cs
@@ -24,7 +24,7 @@ namespace ResourceServices.Service
                 var company = from com in db.Company
                               join acc in db.Account on com.CompanyId equals acc.CompanyId
                               where com.IsActive == isActive
-                              select new CompanyDTO(com, acc.Fullname);
+                              select new CompanyDTO(acc.Company, acc.Fullname);
                 return company.ToList();
             }
         }
