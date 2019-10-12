8a7fdbc31ed42854101c571d5712e28f378e9bc3
Hai Nguyen <hainv54@fpt.com.vn>
2019-10-12 19:46:46 +0700

    add Producer rabbitmq in resourceservices

---
+++ ResourceServices/ResourceServices/RabbitMQ/Producer.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResourceServices.RabbitMQ
+{
+    public class Producer
+    {
+        public void PublishMessage(string message, string exch)
+        {
+            var factory = new ConnectionFactory() { HostName = "35.240.253.45" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: exch, type: ExchangeType.Fanout);
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: exch,
+                                     routingKey: "",
+                                     basicProperties: null,
+                                     body: body);
+            }
+        }
+    }
+}
