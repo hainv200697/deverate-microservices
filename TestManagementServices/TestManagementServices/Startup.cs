6d2eb66eb4da5d10be7c1ddb981d24c51e2313fe
AnhNH <gdlinken1@gmail.com>
2019-10-13 01:46:08 +0700

    fix bug + add consumer + refactor code

--- TestManagementServices/TestManagementServices/Startup.cs
+++ TestManagementServices/TestManagementServices/Startup.cs
@@ -10,11 +10,14 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Steeltoe.Discovery.Client;
 using Swashbuckle.AspNetCore.Swagger;
+using TestManagementServices.Model;
 using TestManagementServices.Models;
+using TestManagementServices.RabbitMQ;
 
 namespace Deverate
 {
@@ -31,6 +34,7 @@ namespace Deverate
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IHostedService>(provider => new Consumer(AppConstrain.gen_test_consumer));
             services.AddDiscoveryClient(Configuration);
             services.AddMvc();
             services.AddCors(c =>
@@ -52,12 +56,9 @@ namespace Deverate
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+
             app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseDiscoveryClient();
             app.UseHttpsRedirection();
