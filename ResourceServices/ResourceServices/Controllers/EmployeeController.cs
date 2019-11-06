2d727396ffe3afacf941fd41b23725f1f77773ed
Nguyen Dinh Thong <thongndse61790@fpt.edu.vn>
2019-11-06 17:45:19 +0700

    add send mail and fix

--- ResourceServices/ResourceServices/Controllers/EmployeeController.cs
+++ ResourceServices/ResourceServices/Controllers/EmployeeController.cs
@@ -49,24 +49,52 @@ namespace ResourceServices.Controllers
         }
 
         [HttpPost("CreateEmployee")]
-        public ActionResult CreateEmployee([FromBody] List<MessageAccount> employee)
+        public ActionResult CreateEmployee([FromBody] List<MessageAccount> ListAccountGenerate)
         {
             try
             {
                 List<string> listemail = new List<string>();
-                foreach (var emp in employee)
+                foreach (var emp in ListAccountGenerate)
                 {
 
                     listemail.Add(emp.Email);
                 }
-                var check = context.Account.Where(x => listemail.Contains(x.Email)).Select(x=> x.Email ).ToList();
+                int? companyId = ListAccountGenerate[0].CompanyId;
+                var check = context.Account.Where(x => listemail.Contains(x.Email) && x.CompanyId == companyId).Select(x=> x.Email ).ToList();
                 if (check.Count > 0)
                 {
                     return BadRequest("Email "+ check[0] +" is Existed");
                 }
                 Producer producer = new Producer();
-                producer.PublishMessage(JsonConvert.SerializeObject(employee), "AccountGenerate");
+                producer.PublishMessage(JsonConvert.SerializeObject(ListAccountGenerate), "AccountGenerate");
                 return Ok(rm.Success("Create success"));
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost("ResendPassword")]
+        public ActionResult ResendPassword([FromBody] List<AccountDTO> ListAccountSendPass)
+        {
+            try
+            {
+                List<string> listUsername = new List<string>();
+                foreach (var emp in ListAccountSendPass)
+                {
+
+                    listUsername.Add(emp.username);
+                }
+                int? companyId = ListAccountSendPass[0].companyId;
+                var check = context.Account.Where(x => listUsername.Contains(x.Username) && x.CompanyId == companyId).Select(x => x.Username).ToList();
+                if (check.Count < listUsername.Count)
+                {
+                    return BadRequest("Account " + check[0] + " is not Existed");
+                }
+                Producer producer = new Producer();
+                producer.PublishMessage(JsonConvert.SerializeObject(listUsername), "AccountGenerate");
+                return Ok(rm.Success("Password was send to your email!"));
             }
             catch (Exception)
             {
@@ -85,7 +113,7 @@ namespace ResourceServices.Controllers
                 }
                 var listEmployee = AccountDAO.UpdateEmployeeStatus(employee);
 
-                return Ok(rm.Success(listEmployee));
+                return Ok(rm.Success("Status is changed"));
             }
             catch (Exception ex)
             {
