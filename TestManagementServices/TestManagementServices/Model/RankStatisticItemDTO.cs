287253ea30e04ac0a25a8fd2eed1cefde78465ef
AnhNH <gdlinken1@gmail.com>
2019-11-09 22:05:11 +0700

    refactor code điểm + validate dữ liệu bên test management (continue..)

--- TestManagementServices/TestManagementServices/Model/RankStatisticItemDTO.cs
+++ TestManagementServices/TestManagementServices/Model/RankStatisticItemDTO.cs
@@ -14,18 +14,44 @@ namespace TestManagementServices.Model
         public DateTime? createDate { get; set; }
         public DateTime? endDate { get; set; }
         public string name { get; set; }
+        public TestedItemDTO tested { get; set; }
+        public TotalEmpItemDTO totalEmp { get; set; }
+
 
 
         public RankStatisticItemDTO() { }
         public RankStatisticItemDTO(int? configId, List<RankDTO> series,
-            DateTime? createDate, DateTime? endDate, string name)
+            DateTime? createDate, DateTime? endDate, string name, TestedItemDTO tested, TotalEmpItemDTO totalEmp)
         {
             this.configId = configId;
             this.series = series;
             this.createDate = createDate;
             this.endDate = endDate;
             this.name = name;
+            this.tested = tested;
+            this.totalEmp = totalEmp;
         }
 
+    }
+    public class TestedItemDTO
+    {
+        public string name = "Employees do test";
+        public int? value { get; set; }
+        public TestedItemDTO() { }
+        public TestedItemDTO(int? value)
+        {
+            this.value = value;
+        }
+    }
+
+    public class TotalEmpItemDTO
+    {
+        public string name = "Total employees";
+        public int? value { get; set; }
+        public TotalEmpItemDTO() { }
+        public TotalEmpItemDTO(int? value)
+        {
+            this.value = value;
+        }
     }
 }
--- TestManagementServices/TestManagementServices/Models/DeverateContext.cs
+++ TestManagementServices/TestManagementServices/Models/DeverateContext.cs
@@ -303,7 +303,8 @@ namespace TestManagementServices.Models
                 entity.Property(e => e.StartTime).HasColumnType("datetime");
 
                 entity.HasOne(d => d.Account)
-                    .WithMany(p => p.Test)
+                    .WithMany
+                    (p => p.Test)
                     .HasForeignKey(d => d.AccountId)
                     .HasConstraintName("FK_Test_Account");
 
