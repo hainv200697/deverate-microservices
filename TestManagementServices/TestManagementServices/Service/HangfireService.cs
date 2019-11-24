55fa180ae2fc0cf93e6c9f201c0adb2c61a977bd
Nữ Nguyễn <ngtuyetnu1997@gmail.com>
2019-11-25 01:07:50 +0700

    job

---
+++ TestManagementServices/TestManagementServices/Service/HangfireService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using TestManagementServices.Models;
+
+namespace TestManagementServices.Service
+{
+
+    public interface IHangfireService
+    {
+        void EvaluateRankAllTestNotSubmit();
+    }
+
+    public class HangfireService : IHangfireService
+    {
+        public void EvaluateRankAllTestNotSubmit()
+        {
+            try
+            {
+                using (DeverateContext context = new DeverateContext())
+                {
+                    var now = DateTime.Now;
+                    var testIds = context.Test
+                        .Include(c => c.Config)
+                        .Where(t => t.IsActive && t.StartTime != null && t.Status == "Doing" && DateTime.Compare(now, t.StartTime.Value.AddMinutes(t.Config.Duration)) > 0 && t.IsActive)
+                        .Select(t => t.TestId)
+                        .ToList();
+                    foreach (int testId in testIds)
+                    {
+                        SystemDAO.AutoSubmit(testId);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error hangfire: " + ex);
+            }
+        }
+    }
+}
