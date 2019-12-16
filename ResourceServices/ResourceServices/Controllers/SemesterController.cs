c19cfb986d8861997871e5c6493f1d49f44199b6
HUY-NGUYEN\huy <huy.nguyen2@wisky.vn>
2019-12-16 22:23:08 +0700

    manage semester

---
+++ ResourceServices/ResourceServices/Controllers/SemesterController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ResourceServices.Model;
+using ResourceServices.Models;
+using ResourceServices.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResourceServices.Controllers
+{
+    [Route("SemesterAPI")]
+    public class SemesterController : Controller
+    {
+        DeverateContext context;
+
+        public SemesterController(DeverateContext context)
+        {
+            this.context = context;
+        }
+
+        [Route("GetAllEmployeeInCompany")]
+        [HttpGet]
+        public ActionResult GetAllEmployeeInCompany(int companyId)
+        {
+            try
+            {
+                List<SemesterDTO> emp = SemesterDAO.getAllEmployeeInCompany(companyId);
+                return Ok(emp);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
---
+++ ResourceServices/ResourceServices/Service/SemesterDAO.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using ResourceServices.Model;
+using ResourceServices.Models;
+using ResourceServices.RabbitMQ;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResourceServices.Service
+{
+    public class SemesterDAO
+    {
+        DeverateContext context;
+
+        public SemesterDAO(DeverateContext context)
+        {
+            this.context = context;
+        }
+
+
+        public static List<SemesterDTO> getAllEmployeeInCompany(int companyId)
+        {
+            using (DeverateContext db = new DeverateContext())
+            {
+                var listEmployee = db.Account.Include(x => x.CompanyRank).Where(x => x.CompanyId == companyId && x.RoleId == 3).Select(x => new SemesterDTO(x)).ToList();
+                return listEmployee;
+            }
+        }
+
+        public static void createSemester(List<int> employeeId, int configId, bool typeTest)
+        {
+            using (DeverateContext db = new DeverateContext())
+            {
+                Producer producer = new Producer();
+                producer.PublishMessage(JsonConvert.SerializeObject(employeeId + "" + configId + "" + typeTest), "GenerateTest");
+
+            }
+        }
+    }
+}
