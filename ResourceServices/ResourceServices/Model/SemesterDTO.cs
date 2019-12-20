a2b6fc4050fd0c0430fa0253b265820e9d1d2a41
HUY-NGUYEN\huy <huy.nguyen2@wisky.vn>
2019-12-20 15:32:10 +0700

    manage semester

---
+++ ResourceServices/ResourceServices/Model/SemesterDTO.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using ResourceServices.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResourceServices.Model
+{
+    public class SemesterDTO
+    {
+        public int accountId { get; set; }
+        public string username { get; set; }
+        public string fullname { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+        public string address { get; set; }
+        public bool gender { get; set; }
+        public int roleId { get; set; }
+        public bool isActive { get; set; }
+        public int? companyId { get; set; }
+        public string rankName { get; set; }
+
+        public SemesterDTO()
+        {
+
+        }
+
+        public SemesterDTO(Account account)
+        {
+            this.accountId = account.AccountId;
+            this.username = account.Username;
+            this.fullname = account.Fullname;
+            this.phone = account.Phone;
+            this.email = account.Email;
+            this.address = account.Address;
+            this.gender = account.Gender;
+            this.roleId = account.RoleId;
+            this.isActive = account.IsActive;
+            this.companyId = account.CompanyId;
+            if (account.CompanyRankId == null)
+            {
+                this.rankName = "Unknow rank";
+            }
+            else
+            {
+                this.rankName = account.CompanyRank.Name;
+            }
+
+        }
+    }
+}
---
+++ ResourceServices/ResourceServices/Service/SemesterDAO.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using ResourceServices.Model;
+using ResourceServices.Models;
+using ResourceServices.RabbitMQ;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResourceServices.Service
+{
+    public class SemesterDAO
+    {
+        DeverateContext context;
+
+        public SemesterDAO(DeverateContext context)
+        {
+            this.context = context;
+        }
+
+
+        public static List<SemesterDTO> getAllEmployeeInCompany(int companyId)
+        {
+            using (DeverateContext db = new DeverateContext())
+            {
+                var listEmployee = db.Account.Include(x => x.CompanyRank).Where(x => x.CompanyId == companyId && x.RoleId == 3).Select(x => new SemesterDTO(x)).ToList();
+                return listEmployee;
+            }
+        }
+    }
+}
