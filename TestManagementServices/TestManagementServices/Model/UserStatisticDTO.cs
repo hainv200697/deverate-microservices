a3ede9f0ffe2d796142d8f77594fa1bf1ce918b0
AnhNH <gdlinken1@gmail.com>
2019-12-28 13:24:32 +0700

    fixbug get overall point statistic

--- TestManagementServices/TestManagementServices/Model/UserStatisticDTO.cs
+++ TestManagementServices/TestManagementServices/Model/UserStatisticDTO.cs
@@ -13,27 +13,20 @@ namespace TestManagementServices.Model
         public string username { get; set; }
         public DateTime? startDate { get; set; }
         public string rank { get; set; }
+        public string potentialRank { get; set; }
         public double overallPoint { get; set; }
         public string configName { get; set; }
         public DateTime? configCreateDate { get; set; }
         public int testId { get; set; }
         public UserStatisticDTO() { }
-        public UserStatisticDTO(int? accountId, string username, DateTime? startDate, double overallPoint, string configName, DateTime? configCreateDate)
-        {
-            this.accountId = accountId;
-            this.username = username;
-            this.startDate = startDate;
-            this.overallPoint = overallPoint;
-            this.configName = configName;
-            this.configCreateDate = configCreateDate;
-        }
 
-        public UserStatisticDTO(int? accountId, string username, DateTime? startDate, string rank, double overallPoint, string configName, int testId)
+        public UserStatisticDTO(int? accountId, string username, DateTime? startDate, string rank, string potentialRank, double overallPoint, string configName, int testId)
         {
             this.accountId = accountId;
             this.username = username;
             this.startDate = startDate;
             this.rank = rank;
+            this.potentialRank = potentialRank;
             this.overallPoint = overallPoint;
             this.configName = configName;
             this.configCreateDate = configCreateDate;
