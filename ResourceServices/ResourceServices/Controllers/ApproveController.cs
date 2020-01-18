cd2b9c48d6684315f7374bf81020ba8a8969bf80
Nguyen Dinh Thong <thongndse61790@fpt.edu.vn>
2020-01-19 04:08:47 +0700

    approve rank for employee

---
+++ ResourceServices/ResourceServices/Controllers/ApproveController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AuthenServices.Model;
+using Microsoft.AspNetCore.Mvc;
+using ResourceServices.Model;
+using ResourceServices.Service;
+
+namespace ResourceServices.Controllers
+{
+    [Route("api/[controller]")]
+    public class ApproveController : Controller
+    {
+        [HttpGet("GetApproveRequest")]
+        public ActionResult GetApproveRequest(int configId)
+        {
+            try
+            {
+                List<ApproveRankDTO> requests = ApproveDAO.GetApproveRequest(configId);
+                return Ok(requests);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpGet("GetActionRequest")]
+        public ActionResult GetActionRequest(int configId,int accountId, bool isApprove)
+        {
+            try
+            {
+                ApproveDAO.actionRequest(configId,accountId,isApprove);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(500);
+            }
+        }
+
+    }
+}
