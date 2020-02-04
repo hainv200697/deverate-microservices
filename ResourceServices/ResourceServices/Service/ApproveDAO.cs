143f84c32449ec68e0021829d5b64065c2007f0f
Nguyen Dinh Thong <thongndse61790@fpt.edu.vn>
2020-02-04 15:15:51 +0700

    update approve

--- ResourceServices/ResourceServices/Service/ApproveDAO.cs
+++ ResourceServices/ResourceServices/Service/ApproveDAO.cs
@@ -15,8 +15,6 @@ namespace ResourceServices.Service
         {
             using (DeverateContext context = new DeverateContext())
             {
-                var newConfig = context.Configuration.OrderByDescending(c => c.CreateDate).FirstOrDefault();
-                List<Test> tests = new List<Test>();
                 var approve = context.Test.Include(x => x.Rank).Include(x => x.Account).ThenInclude(x => x.Rank)
                     .Where(x => x.ConfigId == configId && x.IsApprove == null && x.FinishTime != null)
                     .Select(x => new ApproveRankDTO
@@ -30,53 +28,23 @@ namespace ResourceServices.Service
                         rankInTestId = x.RankId,
                         rankInTestName = x.Rank.Name,
                     }).ToList();
-                if (newConfig.ConfigId != configId)
-                {
-                    var newApprove = context.Test.Include(x => x.Rank).Include(x => x.Account).ThenInclude(x => x.Rank)
-                    .Where(x => x.ConfigId == newConfig.ConfigId && x.IsApprove == null && x.FinishTime != null)
-                    .Select(x => new ApproveRankDTO
-                    {
-                        testId = x.TestId,
-                        accountId = x.AccountId,
-                        fullname = x.Account.Fullname,
-                        username = x.Account.Username,
-                        accountRankId = x.Account.RankId,
-                        accountRankName = x.Account.Rank.Name,
-                        rankInTestId = x.RankId,
-                        rankInTestName = x.Rank.Name,
-                    }).ToList();
-                    foreach(var newItem in newApprove)
-                    {
-                        foreach (var item in approve.ToList())
-                        {
-                            if(item.accountId == newItem.accountId)
-                            {
-                                Test test = new Test();
-                                test.TestId = item.testId;
-                                test.IsApprove = false;
-                                tests.Add(test);
-                                approve.Remove(item);
-                            }
-                        }
-                    }
-                }
-                context.UpdateRange(tests);
-                context.SaveChanges();
+
                 return approve;
             }
         }
 
-        public static void ActionRequest(int configId,int accountId, bool isApprove)
+        public static void ActionRequest(int configId, int accountId, bool isApprove)
         {
             using (DeverateContext context = new DeverateContext())
             {
-                Test test = context.Test.Include(x=>x.Account).SingleOrDefault(x => x.ConfigId == configId && x.AccountId == accountId);
+                Test test = context.Test.Include(x => x.Account).SingleOrDefault(x => x.ConfigId == configId && x.AccountId == accountId);
                 test.IsApprove = isApprove;
                 if (isApprove)
                 {
                     test.Account.RankId = test.RankId;
                 }
                 context.Test.Update(test);
+                context.Test.Where(x => x.ConfigId < test.ConfigId && x.AccountId == accountId && x.IsApprove == null).ToList().ForEach(x=> x.IsApprove = false);
                 context.SaveChanges();
             }
         }
